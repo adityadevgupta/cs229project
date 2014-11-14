6349fcfc2bcfabbfc7d19b1bf7a46bef547a7e4e
Aditya Dev Gupta <adityadev93@gmail.com>
2014-11-14 01:32:22 -0800

    defined midi to stream

--- src/util.py
+++ src/util.py
@@ -1,1 +1,15 @@
-# choothiya
+import os
+from music21 import *
+import numpy as np
+
+def midi_to_stream(filename_path):
+	"""
+		Function: midi_to_stream:
+		converts a full filename to a Music21 usable Stream object, 
+	"""
+
+	midi_file = midi.midiFile()
+	midi_file.open()
+	midi_file.read()
+	midi_fileg.close()
+	return midi.translate.midiFileToStream(mf)
