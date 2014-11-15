939f87a4b594d1d5dc30d274590e3657b7284dc0
Aditya Dev Gupta <adityadev93@gmail.com>
2014-11-15 00:58:42 -0800

    trying to add get_chords_from_measure

--- src/test.py
+++ src/test.py
@@ -1,12 +1,3 @@
 from util import *
 
 s = midi_to_stream("../midi/take_5_jh.mid")
-
-def find_measure(stream):
-	if not hasattr(stream, '__iter__'):#not iterable
-		return
-	for e in stream:
-		# if type(e) is Measure:
-		# 	print 'found measure'
-		print(type(e))
-		find_measure(e)
--- src/util.py
+++ src/util.py
@@ -2,6 +2,28 @@ import os
 from music21 import *
 import numpy as np
 
+def get_training_point(measure):
+
+
+def get_chords_from_measure(measure):
+  chords = []
+  for elem in measure:
+    if type(elem) is chord.Chord:
+      chords += elem
+    if type(elem) is stream.Voice:
+      for thing in elem:
+        if type(elem) is chord.Chord:
+          chords += thing
+  return chords
+
+def collapse_measure(measure):
+   
+
+def add_measures_to_parts(score):
+  for part in score:
+    if not part.hasMeasures():
+      part.makeMeasures(inPlace=True)
+
 def midi_to_stream(filename_path):
 	"""
 		Function: midi_to_stream:
@@ -13,6 +35,4 @@ def midi_to_stream(filename_path):
 	midi_file.read()
 	midi_file.close()
 	s = midi.translate.midiFileToStream(midi_file)
-	if not s.hasMeasures():
-		s.makeMeasures(inPlace=True)
 	return s
