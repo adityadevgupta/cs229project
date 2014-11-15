beb47a7db004384228043e646132b697d2e31f94
Aditya Dev Gupta <adityadev93@gmail.com>
2014-11-14 22:19:33 -0800

    added test code for measures

--- src/test.py
+++ src/test.py
@@ -1,1 +1,12 @@
-# chutiya bl'dy
+from util import *
+
+s = midi_to_stream("../midi/autumn_leaves_jpa.mid")
+
+def find_measure(stream):
+	if not hasattr(stream, '__iter__'):#not iterable
+		return
+	for e in stream:
+		# if type(e) is Measure:
+		# 	print 'found measure'
+		print(type(e))
+		find_measure(e)
