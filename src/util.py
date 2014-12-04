24b8bfd14e140c56d929c47cd462bb6510d1de94
Aditya Dev Gupta <adityadev93@gmail.com>
2014-12-04 15:50:47 -0800

    began get training data method

--- src/util.py
+++ src/util.py
@@ -2,8 +2,55 @@ import os
 from music21 import *
 import numpy as np
 
+"""
+object
+
+Score (music21 score type)
+bass
+treble
+nParts
+nMeasures
+time signature
+
+"""
+
+def get_score_object(score):
+  return None
+
+def get_training_data(score):
+
+  # basic variables
+  nParts = len(score)
+  treble = []
+  bass = []
+  nMeasures = min([len(s[i]) for i in range(nParts)])
+
+  # determine clefs of parts
+  for i in range(nParts):
+    if type(s[i][0][0]) is clef.TrebleClef:
+      treble += i
+    if type(s[0][0][0]) is clef.BassClef:
+      bass += i
+
+  X_train = []
+
+  # get training point for every measure
+  for i in range(nMeasures):
+    x = [None]*3
+    # not the first measure
+    if i != 0:
+      # for each bass clef part
+      for b in bass:
+        chords = get_chords_from_measure(score[b][0])
+
+def get_solo_part(score, treble):
+  return None
+
 def get_training_point(measure):
 	return None
+
+def get_time_signature(measure):
+  return s[0][0][1]
 
 def get_chords_from_measure(measure):
   chords = []
@@ -16,7 +63,6 @@ def get_chords_from_measure(measure):
           chords += [thing]
   return chords
    
-
 def add_measures_to_parts(score):
   for part in score:
     if not part.hasMeasures():
