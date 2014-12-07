c3e8ea11ffedc3cfc55247d7b1a568d1be0eaed4
Aditya Dev Gupta <adityadev93@gmail.com>
2014-12-06 20:52:27 -0800

    fixed bugs, still have that thing where the prev and next measures are only inputting notes and not chords

--- src/util.py
+++ src/util.py
@@ -32,10 +32,10 @@ def midi_to_stream(filename_path):
 		Function: midi_to_stream:
 		converts a full filename to a Music21 usable Stream object, 
 	"""
-
-	midi_file = midi.MidiFile()
-	midi_file.open(filename_path)
-	midi_file.read()
-	midi_file.close()
-	s = midi.translate.midiFileToStream(midi_file)
+	# midi_file = midi.MidiFile()
+	# midi_file.open(filename_path)
+	# midi_file.read()
+	# midi_file.close()
+	s = midi.translate.midiFilePathToStream(filename_path)
+	
 	return s
